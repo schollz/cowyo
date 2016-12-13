483d698f3a494c2259b9a464ff1501f5cc455c65
Zack Scholl <zack.scholl@gmail.com>
2016-12-12 19:57:33 -0800

    Fix check origin

--- websockets.go
+++ websockets.go
@@ -12,6 +12,9 @@ import (
 var wsupgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 func wshandler(w http.ResponseWriter, r *http.Request) {
