5fab2749f95c77f3ed0eb51252324637b8393343
Zack Scholl <zack.scholl@gmail.com>
2017-03-24 06:25:59 -0600

    Add reverse list
    Former-commit-id: f45b4f86ca434997cc1d9c9263818b6064aa44f9

--- utils_test.go
+++ utils_test.go
@@ -4,6 +4,16 @@ import (
 	"testing"
 )
 
+func TestReverseList(t *testing.T) {
+	s := []int64{1, 10, 2, 20}
+	if reverseSliceInt64(s)[0] != 20 {
+		t.Errorf("Could not reverse: %v", s)
+	}
+	s2 := []string{"a", "b", "d", "c"}
+	if reverseSliceString(s2)[0] != "c" {
+		t.Errorf("Could not reverse: %v", s2)
+	}
+}
 func TestHashing(t *testing.T) {
 	p := HashPassword("1234")
 	log.Debug(p)
